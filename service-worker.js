26b797fcb09e5c52712393d4e3932c21d2ea0643
amuuuune <184851616+amuuuune@users.noreply.github.com>
2026-05-07 21:26:55 +0900

    Normalize app spacing

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "overtime-app-v28";
+const CACHE_NAME = "overtime-app-v29";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=28",
-  "./app.js?v=28",
+  "./styles.css?v=29",
+  "./app.js?v=29",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
