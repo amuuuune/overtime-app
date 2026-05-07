4bb5a96e003acdbf9d0d02bc0c120bf7bfc391c8
amuuuune <184851616+amuuuune@users.noreply.github.com>
2026-05-07 23:19:41 +0900

    Unify vertical spacing rhythm

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "overtime-app-v29";
+const CACHE_NAME = "overtime-app-v31";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=29",
-  "./app.js?v=29",
+  "./styles.css?v=31",
+  "./app.js?v=31",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
