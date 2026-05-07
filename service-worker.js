894dd761d01eafdd0442a9d672f55283b3d6a252
amuuuune <184851616+amuuuune@users.noreply.github.com>
2026-05-07 20:59:01 +0900

    Remove extra helper text

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "overtime-app-v26";
+const CACHE_NAME = "overtime-app-v27";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=26",
-  "./app.js?v=26",
+  "./styles.css?v=27",
+  "./app.js?v=27",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
